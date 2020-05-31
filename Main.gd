6afa9caf1bd352af4f70a4d2dd6465166365d841
nata8ify@gmail.com <you@example.com>
2020-05-31 23:36:35 +0700

    Initial Project

---
+++ Main.gd
@@ -0,0 +1,69 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+
+	
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
+	pass # Replace with function body.
+
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+	pass # Replace with function body.
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	pass # Replace with function body.
+
+
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.offset = randi()
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
---
+++ Player.gd
@@ -0,0 +1,71 @@
+extends Area2D
+
+signal hit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _process(delta):
+	var valocity = Vector2()
+	if (Input.is_action_pressed("ui_up")):
+		valocity.y -= 1
+	
+	if (Input.is_action_pressed("ui_down")):
+		valocity.y += 1
+		
+	if (Input.is_action_pressed("ui_left")):
+		valocity.x -= 1
+		
+	if (Input.is_action_pressed("ui_right")):
+		valocity.x += 1
+		
+	if (valocity.length() > 0):
+		valocity = valocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += valocity * delta
+	position.x = _sim_clamp(position.x, 0, screen_size.x)
+	position.y = _sim_clamp(position.y, 0, screen_size.y)
+	
+	if valocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = valocity.x < 0
+	if valocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.flip_v = valocity.y > 0
+		
+func _sim_clamp(value, value_min, value_max):
+	if (value < value_min):
+		return value_min
+	if (value_max < value):
+		return value_max
+	return value;
+
+
+func _on_Player__body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disable", true) # Replace with function body.
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
